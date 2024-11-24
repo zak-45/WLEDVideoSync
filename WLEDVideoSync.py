37c1f84722fcf5d56005ba625e4ba98cae74cbdb
zak-45 <zak-45600@gmail.com>
2024-11-24 23:58:31 +0100

    nuitka 2.5

--- WLEDVideoSync.py
+++ WLEDVideoSync.py
@@ -1,6 +1,4 @@
 # Compilation mode, standalone everywhere, except on macOS there app bundle
-# nuitka-project-if: {OS} in ("Windows", "Linux", "FreeBSD"):
-#    nuitka-project: --onefile
 # nuitka-project-if: {OS} == "Darwin":
 #    nuitka-project: --standalone
 #    nuitka-project: --macos-create-app-bundle
