de7a4d7302bb7118d8dad3862ffc895709f64273
zak-45 <zak-45600@gmail.com>
2025-03-02 15:34:59 +0100

    some demo

--- xtra/coldtype/cv2_demo_01.py
+++ xtra/coldtype/cv2_demo_01.py
@@ -2,15 +2,12 @@
 # importing cv2
 import cv2
 
-# path
+# image path
 path = r"./splash-screen.png"
-
 # Reading an image in grayscale mode
 image = cv2.imread(path, 0)
-
 # Window name in which image is displayed
-window_name = 'image'
-
+window_name = 'cv2_demo_01 preview'
 # Using cv2.imshow() method
 # Displaying the image
 cv2.imshow(window_name, image)
